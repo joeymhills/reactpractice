2f648bdf0e56fb66859417c384464007d47162dd
Joseph Hills <hillsj3@southernct.edu>
2023-03-26 23:46:55 -0400

    transfer from other file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import homepage from './homepage.jpg'
+import webdesign from './webdesign.gif'
+import laptop from './laptop.jpg'
 import { Component } from 'react';
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-          <img src={logo} className="header-logo" alt = " " />
+         <div className= "header-logo">
+            <h1>JMH</h1>
+            <p>web design</p>
+         </div>
           <div className="nav-box">
             <div className="nav-bar">
               <a id="home" href="./src/App.js">Home</a>
+              <a id="about" href="./src/about"> About</a>
               <a id="projects" href="./src/projects">Projects</a>
-              <a id="contact" href="./src/contact">Contact Us</a>
+              <a id="contact" href="./src/contact">Contact</a>
             </div>
           </div>
           <div className="request-button-div">
-            <button type="button" className="request-button">Contact me!</button>
+            <button type="button" className="request-button">Request</button>
           </div>
       </header>
       <div className="homepage-content">
-        <img src={homepage} className="homepage-img" alt=" " />
+        <h1 id="welcome-text"> Welcome to my website</h1>
+        <div className="laptop-box">
+          <img src={laptop} id="laptop" alt=" " />
+        </div>
       </div>
+      <img src={webdesign} className="web-design"  alt= " " />
+
     </div>
   );
 }
