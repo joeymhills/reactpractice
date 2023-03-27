ceeab10cb5050c23f70ea42e6cf9696de5f80da0
Joseph Hills <hillsj3@southernct.edu>
2023-03-27 12:31:34 -0400

    fulcum, commit..... yuuuuhhhhhhhh

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
 import headshot from './headshot.png'
 import laptop from './laptop.jpg'
-import { Component } from 'react';
+import { useEffect } from 'react';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-  window.addEventListener("scroll", reveal);
-}
 
 
 function App() {
-  reveal();
+  useEffect(()=> {
+    AOS.init({duration:2000})
+  },[]);
   return (
     <div className="app">
       <header className="app-header">
@@ -33,7 +21,7 @@ function App() {
             <div className="nav-bar">
               <a id="home" href="./src/App.js">Home</a>
               <a id="about" href="./src/about"> About</a>
-              <a id="projects" href="./src/projects">Projects</a>
+              <a id="projects" href="./src/projects">Services</a>
               <a id="contact" href="./src/contact">Contact</a>
               <div className="request-button-div">
                 <button type="button" className="request-button">Request</button>
@@ -43,17 +31,24 @@ function App() {
           
       </header>
       <div className="homepage-content">
-        <h1 id="welcome-text"></h1>
+        <h1 className="flex-box1"></h1>
+        <h1 className="flex-box2"></h1>
+        <h1 className="welcome-text"></h1>
       </div>
+
       <div className="about-page">
         <h1 className="about-heading">Hi, i'm Joey</h1>
-        <img src={headshot} className="headshot" alt = " " />
-        <p className="about-p">I'm a computer science student with a passion for coding. I create
+        <img src={headshot} className="headshot" data-aos="fade-up" alt = " " />
+        <p className="about-p">I'm a computer science student with a passion for coding.<br />I create
           custom, responsive, and affordable websites.
         </p>
+      </div>
+      <div className="services-page">
+        <h1 className="services-heading">Services</h1>
       </div>
     </div>
   );
 }
+
 
 export default App;
